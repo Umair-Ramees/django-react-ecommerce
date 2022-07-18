0c1d0b2f469933fa5f3906158fe82c4daa08b369
Nabi <46531267+nabi1020-dev@users.noreply.github.com>
2022-07-18 23:13:40 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Hoc from "./hoc/hoc";
+import Hoc from "./hoc/hoc"; 
 
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
