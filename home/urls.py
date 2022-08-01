730f797d38d993219948a4c6e72245ed6bd0ad96
Nabi <46531267+nabi-topdev@users.noreply.github.com>
2022-08-01 20:43:33 +0800

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+ 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
